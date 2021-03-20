b830f7e1f47a4512fb05d0b910807a78fe73b660
bryanlin16899@outlook.com <bryanlin16899@outlook.com>
2021-03-21 02:19:30 +0800

    update 2021.03.21

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-import datetime
+from twilio.rest import Client
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -8,6 +8,8 @@ STOCK_API_KEY = "DH40O164TFMIFCWS"
 NEWS_API_KEY = "eac758f36bd5438fbb52614272b68601"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+TWILIO_SID = "AC54eaf9f436c566420931df8dbbb038a8"
+TWILIO_AUTH_TOKEN = "0d8456dcb3755e650a5a8e30dc22bd29"
 
 stock_params = {
     "function": "TIME_SERIES_DAILY",
@@ -15,44 +17,38 @@ stock_params = {
     "apikey": STOCK_API_KEY
 }
 
-time = datetime.date.today()
-y_day = time - datetime.timedelta(days=1)
-b_y_day = y_day - datetime.timedelta(days=1)
-
 response = requests.get(url=STOCK_ENDPOINT, params=stock_params)
-data = response.json()
-y_price = float(data["Time Series (Daily)"][f"{y_day}"]["4. close"])
-b_y_price = float(data["Time Series (Daily)"][f"{b_y_day}"]["4. close"])
-diff_abs = abs(y_price - b_y_price)
-percentage = (diff_abs / y_price)*100
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then int("Get News").
-if percentage > 5:
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+y_data = data_list[0]
+y_price = float(y_data["4. close"])
 
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+b_y_data = data_list[1]
+b_y_price = float(b_y_data["4. close"])
+diff = y_price - b_y_price
+up_down = None
+if diff > 0:
+    up_down = "Up"
+else:
+    up_down = "Down"
+percentage = round((diff / y_price)*100)
 
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+news_params = {
+    "qInTitle": COMPANY_NAME,
+    "apiKey": NEWS_API_KEY
+}
 
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+if abs(percentage) > 1:
+    news_response = requests.get(url=NEWS_ENDPOINT, params=news_params)
+    data = news_response.json()
+    top3_news = data["articles"][0:3]
+    news_list = [ f"{STOCK_NAME}: {up_down}{percentage}Hedaline: {article['title']}. \nBrief: {article['description']}" for article in top3_news]
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
 
-
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
-
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-#TODO 9. - Send each article as a separate message via Twilio. 
-
-
-
-#Optional TODO: Format the message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+    for article in news_list:
+        message = client.messages.create(
+            body=article,
+            from_="+13603287144",
+            to="+886972988302"
+        )
 
